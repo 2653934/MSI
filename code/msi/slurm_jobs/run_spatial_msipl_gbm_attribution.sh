5bcb0c003c2f48c9e1b8812e6242b10651e67a2b
Zayd <2653934@students.wits.ac.za>
2026-09-20 11:36:37 +0200

    Fix node issue

--- code/msi/slurm_jobs/run_spatial_msipl_gbm_attribution.sh
+++ code/msi/slurm_jobs/run_spatial_msipl_gbm_attribution.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=04:00:00
 #SBATCH --mem=48G
 #SBATCH --exclusive
-#SBATCH --exclude=mscluster44,mscluster45,mscluster48,mscluster50,mscluster51,mscluster57,mscluster62,mscluster65,mscluster74,mscluster83
+#SBATCH --exclude=mscluster44,mscluster45,mscluster48,mscluster50,mscluster51,mscluster57,mscluster62,mscluster65,mscluster74,mscluster75,mscluster83
 #SBATCH --output=logs/gbm-ig-%j.out
 #SBATCH --error=logs/gbm-ig-%j.err
 
